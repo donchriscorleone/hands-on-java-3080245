48a9c9abe14a4cb988dc0e2070526723c263222b
christopherii <christopheriilajom@gmail.com>
2022-11-23 00:33:19 +0000

    Added customer class.

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,48 @@
+package bank;
+
+public class Customer {
+  private int id;
+  private String name;
+  private String password;
+  private int accountId;
+
+  public Customer(int id, String name, String password, int accountId) {
+    setId(id);
+    setName(name);
+    setPassword(password);
+    setAccountId(accountId);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getAccountId() {
+    return this.accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+
+}
